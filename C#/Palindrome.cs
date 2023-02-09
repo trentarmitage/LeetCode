9b34f8a340bbd40bac753fd0353a14a5b7570dac
trentarmitage <trentarmitage@live.com>
2023-02-09 16:01:37 -0600

    Add Palindrome Solution

---
+++ C#/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        // Check to see if Integer is a postive number
+
+        if (x >= 0)
+        {
+            Console.WriteLine("Integer is greater than zero");
+            // Convert to String
+            string palindrome = x.ToString();
+
+            int a = 0;
+            int b = palindrome.Length - 1;
+
+            while (a < b)
+            {
+                if (palindrome[a] != palindrome[b])
+                {
+                    return false;
+                }
+                a++;
+                b--;
+            }
+
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Integer is a negative number");
+            return false;
+        }
+    }
+}
